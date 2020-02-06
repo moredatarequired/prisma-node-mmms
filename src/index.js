116118ef024a341bf59c0310c7c178cda71954d9
Hugh Wimberly <hugh.wimberly@gmail.com>
2020-02-06 15:07:07 -0500

    Add resolvers for nested fields

--- src/index.js
+++ src/index.js
@@ -1,34 +1,42 @@
-const { GraphQLServer } = require('graphql-yoga')
-const { prisma } = require('./generated/prisma-client')
+const { GraphQLServer } = require("graphql-yoga");
+const { prisma } = require("./generated/prisma-client");
 
 const resolvers = {
   Query: {
-    member: (root, args, context, info) => {
-      return context.prisma.member({_id: args.id})
-    },
-    policy: (root, args, context, info) => {
-      return context.prisma.policy({_id: args.id})
-    },
+    member: (root, args, ctx) => ctx.prisma.member({ _id: args.id }),
+    policy: (root, args, ctx) => ctx.prisma.policy({ _id: args.id }),
+    group: (root, args, ctx) => ctx.prisma.group({ _id: args.id }),
+    company: (root, args, ctx) => ctx.prisma.company({ _id: args.id })
   },
   Member: {
-    policies: (parent, args, context, info) => {
-      console.log(parent)
-      return parent.policies
-    },
+    policies: (parent, args, ctx) =>
+      ctx.prisma.member({ _id: parent._id }).policies(),
+    beneficiaryOf: (parent, args, ctx) =>
+      ctx.prisma.member({ _id: parent._id }).beneficiaryOf()
   },
-  // Mutation: {
-  //   post: (root, args, context) => {
-  //     return context.prisma.createLink({
-  //       url: args.url,
-  //       description: args.description,
-  //     })
-  //   },
-  // },
-}
+  Policy: {
+    groupContract: (parent, args, ctx) =>
+      ctx.prisma.policy({ _id: parent._id }).groupContract(),
+    subscriber: (parent, args, ctx) =>
+      ctx.prisma.policy({ _id: parent._id }).subscriber(),
+    beneficiaries: (parent, args, ctx) =>
+      ctx.prisma.policy({ _id: parent._id }).beneficiaries()
+  },
+  Group: {
+    company: (parent, args, ctx) =>
+      ctx.prisma.group({ _id: parent._id }).company(),
+    policies: (parent, args, ctx) =>
+      ctx.prisma.group({ _id: parent._id }).policies()
+  },
+  Company: {
+    groupContracts: (parent, args, ctx) =>
+      ctx.prisma.company({ _id: parent._id }).groupContracts()
+  }
+};
 
 const server = new GraphQLServer({
-  typeDefs: './src/schema.graphql',
+  typeDefs: "./src/schema.graphql",
   resolvers,
-  context: { prisma },
-})
-server.start(() => console.log(`Server is running on http://localhost:4000`))
+  context: { prisma }
+});
+server.start(() => console.log(`Server is running on http://localhost:4000`));
